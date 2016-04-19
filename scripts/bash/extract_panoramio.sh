96ecabf66f7213edec38e6c4fdfed4431f1138be
bogosmith <kovacbo@s-estat-bgdnode-01.eurostat.cec>
2016-04-19 09:21:57 +0200

    Now getting all pictures metadata.

--- scripts/bash/extract_panoramio.sh
+++ scripts/bash/extract_panoramio.sh
@@ -53,8 +53,25 @@ maxlon=$(echo $lon+0.007 | bc)
 printf %f"\n"%f"\n"%f"\n"%f"\n" $minlat $maxlat $minlon $maxlon
 
 # mapfilter=true would remove many pictures for the purpose of not showing overlapping ones
-wget -O ${outdir}/file1.json http://www.panoramio.com/map/get_panoramas.php?set=full\&from=0\&to=100\&minx=$minlon\&miny=$minlat\&maxx=$maxlon\&maxy=$maxlat\&size=original\&mapfilter=false 
-# beautify
-cat ${outdir}/file1.json | python -mjson.tool > ${outdir}/data
+#wget -O ${outdir}/file1.json http://www.panoramio.com/map/get_panoramas.php?set=full\&from=0\&to=100\&minx=$minlon\&miny=$minlat\&maxx=$maxlon\&maxy=$maxlat\&size=original\&mapfilter=false 
 
+wget -O ${outdir}/initial.json http://www.panoramio.com/map/get_panoramas.php?set=full\&from=0\&to=0\&minx=$minlon\&miny=$minlat\&maxx=$maxlon\&maxy=$maxlat\&size=original\&mapfilter=false 
+# beautify
+cat ${outdir}/initial.json | python -m json.tool > ${outdir}/initial.pretty
+cnt=$(cat ${outdir}/initial.pretty | grep "\"count\":" | awk '{print $2}' | tr -d ',')
+#cnt=101
+pages=$((cnt/100))
+inexact=$((cnt%100!=0))
+echo $cnt
+echo $pages
+for i in $(seq 0 $((pages+inexact-1)));
+do
+next=$(((i+1)*100))
+low_inclusive=$((i*100))
+high_exclusive=$((next<=cnt-1?next:cnt))
+echo $low_inclusive" -> "$high_exclusive
+wget -O ${outdir}/file$i.json http://www.panoramio.com/map/get_panoramas.php?set=full\&from=$low_inclusive\&to=$high_exclusive\&minx=$minlon\&miny=$minlat\&maxx=$maxlon\&maxy=$maxlat\&size=original\&mapfilter=false 
+cat ${outdir}/file$i.json | python -m json.tool > ${outdir}/file$i.pretty
+sleep $((RANDOM % 3))
+done
 

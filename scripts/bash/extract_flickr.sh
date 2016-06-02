b7e7c7d6f40cf619952e27aa7129d54cd3d1ad29
bogosmith <kovacbo@s-estat-bgdnode-01.eurostat.cec>
2016-06-02 22:20:36 +0200

    Extracting flickr with the new bounding box.

--- scripts/bash/extract_flickr.sh
+++ scripts/bash/extract_flickr.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 -o <output_dir> -a <latitude> -g <longitude> -s <start_date> -e <end_date> (-f <hour|day|week|month>)" 1>&2; exit 1; }
+usage() { echo "Usage: $0 -o <output_dir> -a <latitude> -g <longitude> -s <start_date> -e <end_date> (-d <bounding_box_side_in_kilometers>) (-f <hour|day|week|month>)" 1>&2; exit 1; }
 nodir() { echo "Output directory has to exist." 1>&2; exit 1; }
 dirnotempty() { echo "Output directory has to be empty." 1>&2; exit 1; }
 datefull() { echo "Today's directory exists. Quitting.." 1>&2; exit 1; }
 
 
-while getopts ":o:a:g:s:e:f:" o; do
+while getopts ":o:a:g:s:e:f:d:" o; do
     case "${o}" in
+       d) 
+          bbox_side="${OPTARG}"
+          ;;
        o) 
           outdir="${OPTARG}"
           ;;
@@ -33,6 +36,10 @@ while getopts ":o:a:g:s:e:f:" o; do
 done	
 shift $((OPTIND-1))
 # $1 now references the first non-option argument supplied to the script
+if [ -z "${bbox_side}" ]; then
+  bbox_side="2"
+fi
+
 if [ -z "${freq}" ]; then
   freq="month"
 fi
@@ -65,10 +72,21 @@ if [ "$(ls -A $outdir)" ]; then
   dirnotempty
 fi
 
-minlat=$(echo $lat-0.0045 | bc)
-maxlat=$(echo $lat+0.0045 | bc)
-minlon=$(echo $lon-0.007 | bc)
-maxlon=$(echo $lon+0.007 | bc)
+box_script=$(dirname $(cd "$( dirname "$0" )" && pwd))/perl/getbox.pl
+echo $lat
+echo $lon
+echo $bbox_side
+bounds=($(echo "$lat" "$lon" "$bbox_side" | perl "$box_script"))
+minlon=${bounds[0]}
+minlat=${bounds[1]}
+maxlon=${bounds[2]}
+maxlat=${bounds[3]}
+echo $minlon $minlat
+
+#minlat=$(echo $lat-0.0045 | bc)
+#maxlat=$(echo $lat+0.0045 | bc)
+#minlon=$(echo $lon-0.007 | bc)
+#maxlon=$(echo $lon+0.007 | bc)
 api_key="a40c536d8c174c7a9499b00587975650"
 
 #printf %f"\n"%f"\n"%f"\n"%f"\n" $minlat $maxlat $minlon $maxlon
@@ -79,6 +97,7 @@ currentstamp=$(date -d "$currentdate" "+%s")
 endstamp=$(date -d "$enddate" "+%s")
 tempfile=${outdir}/tempfile
 seriesfile=${outdir}/series
+
 echo $freq $minlon $minlat $maxlon $maxlat >> $seriesfile
 
 while true;
